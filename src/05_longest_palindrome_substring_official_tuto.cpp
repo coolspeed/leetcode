a8a56409b5e4642435deed7b016cf6b935669b6c
coolspeed <coolspeed@coolspeedui-MacBook-Air.local>
2015-05-03 22:27:08 +0900

    05 O(N)

--- src/05_longest_palindrome_substring_official_tuto.cpp
+++ src/05_longest_palindrome_substring_official_tuto.cpp
@@ -5,12 +5,10 @@ http://articles.leetcode.com/2011/11/longest-palindromic-substring-part-i.html
 Among my google result, it was the best study material I had.
 It was short and simple, that I didn't feel any need to do additional work.
 I decided to paste it here and re-implement myself months later.
-
-Now I would go to study the O(N) solution:
-http://articles.leetcode.com/2011/11/longest-palindromic-substring-part-ii.html
 */
 
 
+/*
 string expandAroundCenter(string s, int c1, int c2) {
   int l = c1, r = c2;
   int n = s.length();
@@ -43,6 +41,75 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         return longestPalindromeSimple(s);
+    }
+};
+
+*/
+
+/* O(N) solution, aka Manacher’s Algorithm.
+cited from:
+http://articles.leetcode.com/2011/11/longest-palindromic-substring-part-ii.html
+*/
+
+
+// Transform S into T.
+// For example, S = "abba", T = "^#a#b#b#a#$".
+// ^ and $ signs are sentinels appended to each end to avoid bounds checking
+
+
+string preProcess(string s) {
+  int n = s.length();
+  if (n == 0) return "^$";
+  string ret = "^";
+  for (int i = 0; i < n; i++)
+    ret += "#" + s.substr(i, 1);
+ 
+  ret += "#$";
+  return ret;
+}
+
+
+string longestPalindrome(string s) {
+  string T = preProcess(s);
+  int n = T.length();
+  int *P = new int[n];
+  int C = 0, R = 0;
+  for (int i = 1; i < n-1; i++) {
+    int i_mirror = 2*C-i; // equals to i' = C - (i-C)
+    
+    P[i] = (R > i) ? min(R-i, P[i_mirror]) : 0;
+    
+    // Attempt to expand palindrome centered at i
+    while (T[i + 1 + P[i]] == T[i - 1 - P[i]])
+      P[i]++;
+ 
+    // If palindrome centered at i expand past R,
+    // adjust center based on expanded palindrome.
+    if (i + P[i] > R) {
+      C = i;
+      R = i + P[i];
+    }
+  }
+ 
+  // Find the maximum element in P.
+  int maxLen = 0;
+  int centerIndex = 0;
+  for (int i = 1; i < n-1; i++) {
+    if (P[i] > maxLen) {
+      maxLen = P[i];
+      centerIndex = i;
+    }
+  }
+  delete[] P;
+  
+  return s.substr((centerIndex - 1 - maxLen)/2, maxLen);
+}
+
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        return longestPalindrome(s);
     }
 };
 
